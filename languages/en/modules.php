2c85cb1584b80fcc50c95da082cfd50d7ae70626
Christof W. Raidler <c.raidler@rad-consulting.ch>
2017-02-24 18:09:59 +0100

    Initial

---
+++ languages/en/modules.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Contao extension for RAD Consulting GmbH
+ *
+ * @copyright  RAD Consulting GmbH 2016
+ * @author     Chris Raidler <c.raidler@rad-consulting.ch>
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+ */
+
+// Backend modules
+$GLOBALS['TL_LANG']['MOD']['events'] = array('Event');
