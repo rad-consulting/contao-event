1ce1c7d8d83802dbf77518c42ed697d17aa8fadc
Christof W. Raidler <c.raidler@rad-consulting.ch>
2017-02-24 17:19:40 +0100

    Initial

---
+++ languages/en/tl_rad_event.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Contao extension for RAD Consulting GmbH
+ *
+ * @copyright  RAD Consulting GmbH 2016
+ * @author     Chris Raidler <c.raidler@rad-consulting.ch>
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+ */
+
+/**
+ * Fields
+ */
+$GLOBALS['TL_LANG']['tl_rad_event']['id'] = array('ID');
+$GLOBALS['TL_LANG']['tl_rad_event']['name'] = array('Event');
+$GLOBALS['TL_LANG']['tl_rad_event']['subject'] = array('Subject');
+$GLOBALS['TL_LANG']['tl_rad_event']['tstamp'] = array('Timestamp');
+$GLOBALS['TL_LANG']['tl_rad_event']['attempt'] = array('Attempt');
+$GLOBALS['TL_LANG']['tl_rad_event']['timeout'] = array('Timeout');
+$GLOBALS['TL_LANG']['tl_rad_event']['status'] = array('Status');
+$GLOBALS['TL_LANG']['tl_rad_event']['argument'] = array('Arguments');
+$GLOBALS['TL_LANG']['tl_rad_event']['error'] = array('Error');
