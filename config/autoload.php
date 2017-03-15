98b8496c928bc12c34a3ed086dd37167b5674496
Christof W. Raidler <c.raidler@rad-consulting.ch>
2017-03-15 18:28:15 +0100

    copyright

--- config/autoload.php
+++ config/autoload.php
@@ -2,7 +2,7 @@
 /**
  * @copyright  RAD Consulting GmbH 2017
  * @author     Chris Raidler <c.raidler@rad-consulting.ch>
- * @license    http://opensource.org/licenses/lgpl-3.0.html
+ * @author     Olivier Dahinden <o.dahinden@rad-consulting.ch>
  */
 
 // Log
